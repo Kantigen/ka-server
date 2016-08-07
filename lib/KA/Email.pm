ebae1064e94647e78d155c7b253283410ef922be
1vasari <me@1vasari.xyz>
2016-08-08 09:35:52 +1000

    Use File::Slurp and hash ref instead.

--- lib/KA/Email.pm
+++ lib/KA/Email.pm
@@ -3,34 +3,30 @@ package KA::Email;
 use Moose;
 
 use Email::Stuff;
+use File::Slurp;
 
 sub send_email {
-    my ($self, %options) = @_;
+    my ($self, $options) = @_;
 
-    my $path = '/home/keno/ka-server/var/email-templates/'.$options{filename};
-    my $to_email = $options{to};
+    my $path = '/home/keno/ka-server/var/email-templates/'.$options->{filename};
+    my $to_email = $options->{to};
 
     if (KA->config->get('develop_mode')) {
         $to_email = KA->config->get('dev_mode_email_recipient');
     }
 
-    if (open my $file, "<", $path) {
-        my $message;
-        {
-            local $/;
-            $message = <$file>;
-        }
-        close $file;
+    if (-e $path) {
+        my $message = File::Slurp->read_file($path);
 
-        unless (ref $options{params} eq 'ARRAY') {
-            $options{params} = [];
+        unless (ref $options->{params} eq 'ARRAY') {
+            $options->{params} = [];
         }
 
-        $message = sprintf($message, @{$options{params}});
+        $message = sprintf($message, @{$options->{params}});
 
         Email::Stuff->from('"Keno Antigen <noreply@kenoantigen.com>"')
             ->to($to_email)
-            ->subject($options{subject})
+            ->subject($options->{subject})
             ->text_body($message)
             ->send;
     }
