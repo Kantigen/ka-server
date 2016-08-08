c595cc9d0441acadeafcf67919106df4c4fba37a
1vasari <me@1vasari.xyz>
2016-08-08 16:51:18 +1000

    Fucked about and got nothing to work.

---
+++ t/tests/UnitTestsFor/KA/Email.pm
@@ -0,0 +1,20 @@
+package UnitTestsFor::KA::Email;
+
+use KA::Email;
+
+use Test::Class::Moose;
+
+sub test_that_something_happens {
+    my ($self) = @_;
+
+    KA::Email->send_email({
+        template => 'test',
+        to       => 'me@1vasari.xyz',
+        subject  => 'Testing Email',
+        params   => [
+            'Hello there!'
+        ]
+    });
+}
+
+1;
